7c445ab1af9fce1b7e431606f1c256ba1de42465
Anthony <92795688+lausan3@users.noreply.github.com>
2024-05-20 01:28:39 -0400

    Feat. Camera Controller and Split Screen (#5)
    * Added Gridbox Prototyping Materials Package
    
    * Added some preliminary settings
    
    * Added MovementController.cs
    
    * Added Enia and Hyetta player prefabs
    
    * Added virtual cameras for Enia and Hyetta
    
    * Added CameraController.cs to initialize Enia and Hyetta cameras to one set of settings
    
    * Added split screen camera
    
    * Added Camera System Prefabs

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,36 @@
+using Cinemachine;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public CinemachineVirtualCamera eniaVirtualCamera;
+    public CinemachineVirtualCamera hyettaVirtualCamera;
+
+    [Header("Camera Settings")] 
+    public float orthoSize = 10f;
+
+    public float nearClippingPlane;
+    public float farClippingPlane = 1000f;
+
+    public Vector3 trackedObjectOffset = new (0f, 0.67f, 0f);
+
+    public float cameraDistance = 40f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        ChangeCameraSettings(eniaVirtualCamera);
+        ChangeCameraSettings(hyettaVirtualCamera);
+    }
+
+    void ChangeCameraSettings(CinemachineVirtualCamera camera)
+    {
+        camera.m_Lens.Orthographic = true;
+        camera.m_Lens.OrthographicSize = orthoSize;
+        camera.m_Lens.NearClipPlane = nearClippingPlane;
+        camera.m_Lens.FarClipPlane = farClippingPlane;
+        var transposer = camera.AddCinemachineComponent<CinemachineFramingTransposer>();
+        transposer.m_TrackedObjectOffset = trackedObjectOffset;
+        transposer.m_CameraDistance = cameraDistance;
+    }
+}
